c8f5440b894516114b5fcf358ebd1d6c66029c10
LaciusTC <lacius43@gmail.com>
2020-07-06 16:38:27 -0500

    Implementacion de la clase Label.

--- src/networklayer/Label.cc
+++ src/networklayer/Label.cc
@@ -14,13 +14,59 @@
 // 
 
 #include "Label.h"
+#include <cstdint>
 
-Label::Label() {
-    // TODO Auto-generated constructor stub
+namespace inet {
 
+Label::Label()
+    : addr()
+    , type(Type::UNDEFINED)
+{ }
+
+Label::Label(uint64_t hi, uint64_t lo) {
+    addr.numeric[1] = hi;
+    addr.numeric[0] = lo;
+    // TODO asignar tipo correcto
+    if (addr.symbol[15] == 255) type = BROADCAST;
+    else if (addr.symbol[15] == 254) type = MULTICAST;
+    else if (!addr.symbol[15]) type = UNDEFINED;
+    else type = UNICAST;
+}
+
+Label::Label(uint8_t value) // Broadcast
+    : addr()
+{
+    addr.symbol[15] = value;
+    // TODO asignar tipo correcto
+    if (value == 255) type = BROADCAST;
+    else if (value == 254) type = MULTICAST;
+    else if (!value) type = UNDEFINED;
+    else type = UNICAST;
 }
 
 Label::~Label() {
-    // TODO Auto-generated destructor stub
 }
 
+const Label Label::UNSPECIFIED_ADDRESS;
+const Label Label::BROADCAST_ADDRESS(255);
+const Label Label::MULTICAST_ADDRESS(254);
+//const Label Label::UNICAST_ADDRESS(1..253);
+
+bool Label::operator==(const Label& label) const { 
+    return addr.numeric[1] == label.addr.numeric[1] 
+    && addr.numeric[0] == label.addr.numeric[0];
+}
+
+Label Label::getPrefix(int prefixLength) { 
+    Label label;
+    if (prefixLength < 8) { 
+        label.addr.numeric[1] = this->addr.numeric[1] & (((1 << (8*prefixLength))-1) << (64-8*prefixLength));
+        label.addr.numeric[0] = 0;
+    } else {
+        label.addr.numeric[0] = this->addr.numeric[0] & (((1 << (8*prefixLength))-1) << (64-8*prefixLength));
+        label.addr.numeric[1] = this->addr.numeric[1];
+    }
+    return label;
+}
+
+} // namespace inet
--- src/networklayer/Label.h
+++ src/networklayer/Label.h
@@ -15,11 +15,58 @@
 
 #ifndef NETWORKLAYER_LABEL_H_
 #define NETWORKLAYER_LABEL_H_
+#include <cstdint>
+#include <string>
+#include <cstdlib>
+
+namespace inet {
 
 class Label {
 public:
-    Label();
+    typedef union address_t {
+        uint8_t symbol[16]; // symbol[15] : root
+        uint64_t numeric[2]; // 0 : lo, 1 : hi
+        address_t() : numeric {0,0} {}
+    } Address;
+    enum Type {
+        UNDEFINED = 0,
+        UNICAST = 1,
+        MULTICAST = 254,
+        BROADCAST = 255
+    };
+protected:
+    Address addr;
+    Type type;
+public:
+    Label(); // Constructor por omision
+    Label(uint64_t, uint64_t); // Constructor numerico
+    Label(uint8_t); // Constructor por prefijo
     virtual ~Label();
+    static const Label UNSPECIFIED_ADDRESS; // Todo cero.
+    static const Label UNICAST_ADDRESS; // 
+    static const Label BROADCAST_ADDRESS; // Byte mas significativo en 1.
+    static const Label MULTICAST_ADDRESS; // Prefijo de direccion multicast.
+    
+    std::string str();
+    Address getLabel() const { return addr; }
+
+    bool assertSize(const char* cad) { return sizeof(cad)-1 == 16; }
+    bool isUnspecified() { return !addr.numeric[0] && !addr.numeric[1]; }
+    bool isUnicast() { return addr.symbol[15] && addr.symbol[15] < 254; }
+    bool isMulticast() { return addr.symbol[15] == 254; }
+    bool isBroadcast() { return addr.symbol[15] == 255; }
+
+    bool operator<(const Label& label) const { return addr.numeric[1] < label.addr.numeric[1]; }
+    bool operator<=(const Label& label) const { return addr.numeric[1] <= label.addr.numeric[1]; }
+    bool operator>(const Label& label) const { return addr.numeric[1] > label.addr.numeric[1]; }
+    bool operator>=(const Label& label) const { return addr.numeric[1] >= label.addr.numeric[1]; }
+    bool operator==(const Label&) const;
+    bool operator!=(const Label& label) const { return !(*this == label); }
+
+    bool matches(const Label& label, int prefixLength) { return false; } // TODO implementar para encaminamiento.
+    Label getPrefix(int);
 };
+
+} // namespace inet
 
 #endif /* NETWORKLAYER_LABEL_H_ */
